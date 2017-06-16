a465952ecb960350df16079b4539221d2094dc74
xupingwei <pingwei.xu@xiaoyeyun.com>
2017-06-16 11:57:00 +0800

    忽略opencv包的build文件夹下所有文件

--- openCVLibrary320/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary320/build/generated/source/aidl/release/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: C:\\Users\\xupingwei\\Documents\\Project\\GithubProject\\OpenCVDemo\\openCVLibrary320\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: C:\\Users\\xupingwei\\Documents\\Project\\OpenCVDemo\\openCVLibrary320\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
